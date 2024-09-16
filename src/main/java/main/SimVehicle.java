63597255eb752cb497f28a409a9f7c797f0e4bb9
Christopher Nieder <christopher.nieder@adesso.de>
2024-09-16 21:16:48 +0200

    implement grid-based distance measurement for better performance

--- src/main/java/main/SimVehicle.java
+++ src/main/java/main/SimVehicle.java
@@ -2,40 +2,102 @@ package main;
 
 import lombok.Data;
 import main.analytics.Analyser;
-import main.communication.CarToCarCommunication;
 import main.communication.CarToXMessage;
+import main.consensus.EpidemicGossipAlgorithm;
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.eclipse.sumo.libtraci.TraCIColor;
+import org.eclipse.sumo.libtraci.TraCIPosition;
 import org.eclipse.sumo.libtraci.Vehicle;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Random;
 
 
 @Data
 public class SimVehicle {
 
     private String vehicleId;
+    private MutablePair<Double,Double> position;
+    private int ageInSteps;
     private VehicleState vehicleState;
     private Queue<CarToXMessage> messageQueue;
 
+    private boolean isTraffic;
+    private int consensusIsTrafficCount;
+    private int consensusIsNoTrafficCount;
+
     public void simulateStep(){
         processMessages();
+
+        simulateConsensus();
+
+        simulateFlocking();
+
         Analyser.updateVehicleResult(this);
+        ageInSteps++;
+    }
+
+    private void simulateConsensus() {
+        if(vehicleState==VehicleState.NOT_SYNCHRONIZED) {
+            setVehicleState(VehicleState.SEARCHING_FOR_CONSENSUS);
+        }
+        EpidemicGossipAlgorithm.sendMessageToRandomNumberOfNeighbors(vehicleId,String.valueOf(isTraffic));
+        if(consensusIsTrafficCount>5) {
+            isTraffic = true;
+            Vehicle.setColor(vehicleId,new TraCIColor(0,255,0));
+            consensusIsTrafficCount=0;
+            consensusIsNoTrafficCount=0;
+            setVehicleState(VehicleState.SYNCHRONIZED);
+        }
+        if(consensusIsNoTrafficCount>5) {
+            isTraffic = false;
+            Vehicle.setColor(vehicleId,new TraCIColor(0,0,255));
+            consensusIsTrafficCount=0;
+            consensusIsNoTrafficCount=0;
+            setVehicleState(VehicleState.SYNCHRONIZED);
+        }
+    }
+
+    private void simulateFlocking() {
+
     }
 
     public SimVehicle(String vehicleId){
         this.vehicleId = vehicleId;
-        this.vehicleState = VehicleState.NOT_SYNCHRONIZED;
+        this.ageInSteps=0;
         this.messageQueue = new LinkedList<>();
+        setVehicleState(VehicleState.NOT_SYNCHRONIZED);
+
+        Random rand = new Random();
+        isTraffic = rand.nextInt(3)>=1;
+        consensusIsTrafficCount=0;
+        consensusIsNoTrafficCount=0;
+    }
+
+    public void setVehicleState(VehicleState vehicleState) {
+        if(this.vehicleState==vehicleState) return;
+        this.vehicleState = vehicleState;
+        switch (vehicleState){
+            case NOT_SYNCHRONIZED: {
+                Vehicle.setColor(vehicleId,new TraCIColor(255,0,0));
+                break;
+            }
+            case SYNCHRONIZED: {
+                if(isTraffic) Vehicle.setColor(vehicleId,new TraCIColor(0,255,0));
+                else Vehicle.setColor(vehicleId,new TraCIColor(0,0,255));
+                break;
+            }
+            case SEARCHING_FOR_CONSENSUS:{
+                Vehicle.setColor(vehicleId,new TraCIColor(0,255,255));
+                break;
+            }
+        }
     }
 
     private void processMessage(CarToXMessage message){
-        System.out.println("Recieved message: "+ message);
-        if(message.getData().equals("Bist du da?")) CarToCarCommunication.sendMessageToVehicle(vehicleId,message.getSender(),"Ja, was?");
-        else {
-            Vehicle.setSpeed(vehicleId,Double.parseDouble(message.getData()));
-            Vehicle.setLaneChangeMode(vehicleId,512);
-            Vehicle.changeLane(vehicleId,2,1.0);
-        }
+        if(Boolean.parseBoolean(message.getData())) consensusIsTrafficCount++;
+        else consensusIsNoTrafficCount++;
     }
 
     private void processMessages(){
